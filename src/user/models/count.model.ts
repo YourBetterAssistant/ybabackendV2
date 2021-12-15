d7d1bc0fa195a6486ff99f116b81f22ab24f8ba0
NotTimIsReal <alastairrintumathew@gmail.com>
2021-12-15 20:07:47 +1100

    found a breaking bug

--- src/user/models/count.model.ts
+++ src/user/models/count.model.ts
@@ -1,12 +1,11 @@
 import * as mongoose from 'mongoose';
-import { isString } from 'util';
 export const count = new mongoose.Schema({
   _id: {
     type: String,
     required: true,
   },
   voiceChannelID: {
-    type: isString,
+    type: String,
     required: true,
   },
 });
