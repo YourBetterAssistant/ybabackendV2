2afc9a4feb30e099b386ed8656eb1d26d3b789fb
NotTimIsReal <alastairrintumathew@gmail.com>
2022-01-13 14:14:26 +1100

    origin:*

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    origin: true,
+    origin: '*',
   });
   const redisStore = connectRedis(Session);
   const redisClient = redis.createClient({
