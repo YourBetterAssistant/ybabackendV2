59ed44f75aa436ccefe6633e3c22cbbae08ced20
NotTimIsReal <alastairrintumathew@gmail.com>
2022-01-17 18:00:07 +1100

    debug

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -39,7 +39,6 @@ export class UserService {
     const response = await Axios.get(
       'http://discord.com/api/v9/users/@me/guilds',
       {
-        method: 'GET',
         headers: {
           Authorization: `Bot ${process.env.BOTTOKEN}`,
         },
@@ -118,11 +117,6 @@ export class UserService {
     } else return HttpStatus.BAD_REQUEST;
   }
   async checkIfUserIsInGuild(user: Users, guildID: string) {
-    console.log(
-      (await this.getMutualGuilds(user.guilds, await this.getBotGuilds())).find(
-        (g) => g.id === guildID,
-      ),
-    );
     return (
       await this.getMutualGuilds(user.guilds, await this.getBotGuilds())
     ).find((g) => g.id === guildID);
